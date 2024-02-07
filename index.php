151875570547c0fafc95aaf13aee6c482a093100
elijahf95 <elijah.fernandez.1485@gmail.com>
2024-02-07 10:07:22 +0800

    Modified UI with css

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>LIBRARY MANAGEMENT SYSTEM</title>
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+	<br>
+		<br>
+			<br>
+				<br>
+					<br>
+						<br>
+							<br>
+	<center>
+	<form class="form" action="db.php" method="POST">
+  <ul class="wrapper">
+    
+    <li style="--i:4;"><input class="input" type="text" placeholder="BOOK ID:" required=""></li>
+    <li style="--i:3;"><input class="input" placeholder="BOOK TITLE:" required="" name="book-title"></li>
+    <li style="--i:2;"><input class="input" type="text" placeholder="AUTHOR:" required="" name="author"></li>
+    <button type="submit" style="--i:1;">Submit</button>
+  </ul>
+</form>
+</center>
+
+</body>
+</html>
